f84fbd09726fc7fd34108a6851b4ff795a4b2efa
calamansi <calamansi@calamansiui-MacBookAir.local>
2021-08-01 15:55:20 +0900

    [추가]p5js 추가

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -2,8 +2,27 @@ import React from "react";
 import { ReactComponent as LogoMain } from "images/LogoMain.svg";
 import styled from "styled-components";
 import { Sdiv, Stext } from "components";
+import Sketch from "react-p5";
 
 export const HomeScreen = () => {
+  let y = 0;
+  let direction = "^";
+
+  const setup = (p5, parentRef) => {
+    p5.createCanvas(200, 200).parent(parentRef);
+  };
+
+  const draw = (p5) => {
+    p5.background(0);
+    p5.fill(255, y * 1.3, 0);
+    p5.ellipse(p5.width / 2, y, 50);
+    if (y > p5.height) direction = "";
+    if (y < 0) {
+      direction = "^";
+    }
+    if (direction === "^") y += 8;
+    else y -= 4;
+  };
   return (
     <S.Body>
       <LogoMain />
@@ -26,6 +45,10 @@ export const HomeScreen = () => {
         <S.UnSelectedH1 h1>UX</S.UnSelectedH1>
         <S.UnSelectedH1 h1>BX</S.UnSelectedH1>
       </Sdiv>
+      <div className="App">
+        <h1>react-p5</h1>
+        <Sketch setup={setup} draw={draw} />
+      </div>
     </S.Body>
   );
 };
